59cc1ef6382c4bca2b33fac4a4e86f804e624593
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-06-15 21:56:30 +0530

    angular service add-product full working impl

---
+++ PHASE1/REACT/first-react-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Hello from './components/demoes/Hello';
+
+function App() {
+
+  return (
+    <div >
+     
+    <h1>Welcome to Amazon.com</h1>
+    <Hello />
+    <Hello />
+
+
+    </div>
+  );
+
+}
+
+export default App;
---
+++ PHASE1/REACT/first-react-app/src/components/demoes/Hello.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class Hello extends Component {
+
+    render() 
+    {
+        // .....
+
+        return (
+        <div style={{"border":"1px solid red"}} >
+        <h2>Hello World</h2>
+        </div>
+        )
+        
+    }
+}
+
+
+export default Hello;
