8dc143145eca38802504ce2d127cc7382c55f88a
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-06-28 19:58:56 +0530

    react routing

--- PHASE1/REACT/redux-demo/src/App.js
+++ PHASE1/REACT/redux-demo/src/App.js
@@ -6,11 +6,12 @@ import CompB from './components/CompB';
 import ProductListRedux from './components/ProductListRedux';
 import ProductForm from './components/ProductForm';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-
+<Header />
       <Routes>
         <Route path="/" element={<ProductListRedux />} />
         <Route path="/add-product" element={<ProductForm />} />
