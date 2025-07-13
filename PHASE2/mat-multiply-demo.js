3083ddf84fd1a46bfd7fb12a98dd5b8048330f6d
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-07-13 19:57:58 +0530

    DSA matrix add mul

---
+++ PHASE2/mat-multiply-demo.js
@@ -0,0 +1,38 @@
+matrixA = [
+    [1, 2],
+    [3, 4]
+];
+matrixB = [
+    [5, 6],
+    [7, 8]
+];
+
+
+function matrixMultiplication(matrixA, matrixB) {
+    const result = [];
+
+    let noOfRows = matrixA.length; // no of rows in A
+    let noOfCols = matrixB[0].length; // no of cols in B
+
+
+    for (let i = 0; i < noOfRows; i++) {
+
+        result.push([]);
+
+
+        for (let j = 0; j < noOfCols; j++) {
+            let sum = 0;
+
+            for (let k = 0; k < matrixA[0].length; k++) {
+                sum = sum + matrixA[i][k] * matrixB[k][j];
+            };           
+            result[i][j] = sum;
+        }
+
+    }
+    return result;
+};
+
+
+matricC = matrixMultiplication(matrixA, matrixB);
+console.log(matricC)
