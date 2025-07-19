9425acfe58f09ebd66e5d9e8b83508f03052142e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-07-19 21:45:40 +0530

    quicksort

--- PHASE2/SORTING/quicksort.js
+++ PHASE2/SORTING/quicksort.js
@@ -1,15 +1,11 @@
 function quickSort(array, low, high) {
 
     if (low < high) {
-
         const pivotIndex = partition(array, low, high);
 
         quickSort(array, low, pivotIndex - 1);
-
         quickSort(array, pivotIndex, high);
-
     }
-
 }
 
 function partition(array, low, high) {
@@ -24,18 +20,16 @@ function partition(array, low, high) {
             i++;
             [array[i], array[j]] = [array[j], array[i]]; // Swap elements 
         }
-
     }
 
     [array[i + 1], array[high]] = [array[high], array[i + 1]]; // One last swap  
     return i + 1;// return the pivot index
-
 }
 
-let test_array = [5,11,13,2,1,6];
+let test_array = [5, 11, 13, 2, 1, 6];
 
 console.log('Given array : ', test_array);
 
-quickSort(test_array,0,test_array.length-1);
+quickSort(test_array, 0, test_array.length - 1);
 //after quick, the array has been transformed in-place.
 console.log('Sorted array after quick sort : ', test_array);
