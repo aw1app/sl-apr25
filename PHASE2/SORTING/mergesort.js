84fadea207ce75219e4c624fc35306d20e2dfec6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-07-19 20:14:39 +0530

    mergesort

---
+++ PHASE2/SORTING/mergesort.js
@@ -0,0 +1,49 @@
+function mergeSort(array) {
+
+    // Base case: 
+    if (array.length <= 1)
+        return array
+
+
+
+    const middleIndex = Math.floor(array.length / 2);
+
+    // Divide the array into two halves 
+    const leftHalf = array.slice(0, middleIndex);
+    const rightHalf = array.slice(middleIndex);
+
+
+    // Recursively sort the left and right halves 
+    const sortedLeft = mergeSort(leftHalf);
+    const sortedRight = mergeSort(rightHalf);
+
+
+    return merge(sortedLeft, sortedRight);
+
+}
+
+function merge(leftArray, rightArray) {
+
+    const mergedArray = [];
+
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+
+    // Merge the two sorted arrays 
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+
+        if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+            mergedArray.push(leftArray[leftIndex]);
+            leftIndex++;
+        } else {
+            mergedArray.push(rightArray[rightIndex]);
+            rightIndex++;
+        };
+
+         // if there are any remaining elements in either of the array
+         return mergedArray.concat(leftArray.slice(leftIndex)).concat(rightArray.slice(rightIndex));
+
+    }
+
+}
