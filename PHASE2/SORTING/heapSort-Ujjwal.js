14eb66898ab4efa715bb424b877b74a510b00d4f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-07-20 21:00:18 +0530

    heapsort

---
+++ PHASE2/SORTING/heapSort-Ujjwal.js
@@ -0,0 +1,40 @@
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    console.log("Initial Max Heap:", [...arr]);
+    
+    for (let i = n - 1; i > 0; i--) {
+        // Swap current root with end
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+ 
+        console.log(`After moving max to index ${i}:`, [...arr]);
+        // Heapify the reduced heap
+        heapify(arr, i, 0);
+    }
+    console.log("Final Sorted Array:", arr);
+}
+
+
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest !== i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, n, largest);
+    }
+}
+
+let arr = [12, 11, 13, 5, 6, 7];
+console.log(arr);
+
+console.log(heapSort(arr));
