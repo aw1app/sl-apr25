f2730efc49a329b5eaed6afc460070832dcc6cff
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-09 22:49:48 +0530

    Graphs

---
+++ PHASE2/DS/GRAPH/Graph.js
@@ -0,0 +1,50 @@
+
+
+class Graph {
+
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    // Add a vertex to the graph
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    // Add an edge to the graph
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    // Remove an edge from the graph
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    }
+
+    // Display the graph
+    printGraph() {
+        for (let vertex in this.adjacencyList) {
+            console.log(vertex + " -> " + this.adjacencyList[vertex].join(", "));
+        }
+    }
+
+}
+
+// sample usage
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.addEdge("C", "A");
+graph.addEdge("C", "D");
+graph.printGraph();
+
