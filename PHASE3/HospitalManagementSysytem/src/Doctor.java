39321bd96649236a37197ff58994483e1aa1ca11
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-17 21:47:41 +0530

    Java constructors demo

--- PHASE3/HospitalManagementSysytem/src/Doctor.java
+++ PHASE3/HospitalManagementSysytem/src/Doctor.java
@@ -1,31 +1,64 @@
 
 public class Doctor {
-	
-	//properties / fields / instance variables
+
+	// properties / fields / instance variables
 	String name;
 	int age;
-	
+
 	long phone;
-	
+
 	int licenseNumber;
-	
+
 	String degree;
 	String specialist;
+
+	/* CONSTRUCTORS 
+	 * (constructors are like methods,
+	 *  whose sole job is to create objects.
+	 *   
+	*/
 	
+	// default constructor
+	Doctor() {
+		
+	}
 	
+	// custom constructors 
+	Doctor(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	Doctor(String name, int age, int licenseNumber) {
+		this(name,age);
+		
+		this.licenseNumber = licenseNumber;
+	}
+	
+	Doctor(String name, int age, int licenseNumber, String specialist) {
+		this(name,age,licenseNumber);		
+		
+		this.specialist = specialist;
+	}
+
 	// methods / actions / behaviors
-	void performSurgery(){
+	void performSurgery() {
 		System.out.println("Starting surgery ");
 	}
-	
-	void diagnose(){
+
+	void diagnose() {
 		System.out.println("diagnosing..");
 	}
-	
-	String advice(){
+
+	String advice() {
 		System.out.println("advising..");
-		
+
 		return "Take rest for 1 week";
+	}
+	
+	
+	public String toString(){
+		return this.name + " , " + this.age + ", "+ this.licenseNumber + ", " + this.specialist;				
 	}
 
 }
--- PHASE3/HospitalManagementSysytem/src/HMSMain.java
+++ PHASE3/HospitalManagementSysytem/src/HMSMain.java
@@ -9,15 +9,26 @@ public class HMSMain {
 		doctor1.licenseNumber = 345345346;
 		doctor1.specialist = "Cardiac Surgeon";
 		
+		System.out.println("Doctor 1 details :  "+doctor1.toString());
+		
 		String docAdvice = doctor1.advice();
 		System.out.println("Advice from doctor 1:  "+docAdvice);
 		
-		Doctor doctor2 = new Doctor(); // doctor class object
-		doctor2.name = "Mohini";
-		doctor2.age = 47;
+		Doctor doctor2 = new Doctor("Mohini", 47); // doctor class object		
 		doctor2.licenseNumber = 56565656;
 		doctor2.specialist = "Dental";
 		
+		System.out.println("Doctor 2 details :  "+doctor2);
+		
+		String docAdvice2 = doctor2.advice();
+		System.out.println("Advice from doctor 2:  "+docAdvice2);
+		
+		
+		Doctor doctor3 = new Doctor("Suman", 41, 343434535, "Dental" ) ;
+		String docAdvice3 = doctor3.advice();
+		
+		System.out.println("Doctor 3 details :  "+doctor3);
+		System.out.println("Advice from doctor 3:  "+docAdvice3);
 		
 		Patient patient1 = new Patient();
 		patient1.age=21;
@@ -27,6 +38,14 @@ public class HMSMain {
 		patient1.bookAppointment(doctor2);
 		
 		//TASK-1: Create a Technican and let that technician do some activity
+		Technician technician = new Technician();
+		technician.name="ravi";
+		technician.age=32;
+		technician.department="electrical work";
+		technician.shift="night";
+		
+		technician.maintainEquipment();
+		
 
 	}
 
