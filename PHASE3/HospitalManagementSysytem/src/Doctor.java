fd58f454161bd6729280525a448cd524c56aea50
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-17 21:51:00 +0530

    Java constructors demo

--- PHASE3/HospitalManagementSysytem/src/Doctor.java
+++ PHASE3/HospitalManagementSysytem/src/Doctor.java
@@ -12,35 +12,38 @@ public class Doctor {
 	String degree;
 	String specialist;
 
-	/* CONSTRUCTORS 
-	 * (constructors are like methods,
-	 *  whose sole job is to create objects.
-	 *   
-	*/
-	
+	/*
+	 * CONSTRUCTORS (constructors are like methods, whose sole job is to create
+	 * objects.
+	 * 
+	 */
+
 	// default constructor
 	Doctor() {
-		
+
 	}
-	
-	// custom constructors 
+
+	// custom constructors
 	Doctor(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
-	
+
 	Doctor(String name, int age, int licenseNumber) {
-		this(name,age);
-		
+		this(name, age);
+
 		this.licenseNumber = licenseNumber;
 	}
-	
+
 	Doctor(String name, int age, int licenseNumber, String specialist) {
-		this(name,age,licenseNumber);		
-		
+		this(name, age, licenseNumber);
+
 		this.specialist = specialist;
 	}
 
+	
+	// TASK-2: Create a custom constructor that will set all the properties of this class.
+	
 	// methods / actions / behaviors
 	void performSurgery() {
 		System.out.println("Starting surgery ");
@@ -55,10 +58,9 @@ public class Doctor {
 
 		return "Take rest for 1 week";
 	}
-	
-	
-	public String toString(){
-		return this.name + " , " + this.age + ", "+ this.licenseNumber + ", " + this.specialist;				
+
+	public String toString() {
+		return this.name + " , " + this.age + ", " + this.licenseNumber + ", " + this.specialist;
 	}
 
 }
