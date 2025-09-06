7f879fe701b17b93510a9c62398b7384969f9f0f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-09-06 19:55:20 +0530

    mongodb demo

--- PHASE3/mongodemo/src/main/java/mongodemo/MongoDBDemo.java
+++ PHASE3/mongodemo/src/main/java/mongodemo/MongoDBDemo.java
@@ -12,26 +12,28 @@ public class MongoDBDemo {
 	private static MongoClient mongoClient;
 	private static MongoDatabase database;
 	private static MongoCollection<Document> productsCollection;
-	
+
 	public static void main(String[] args) {
-		
+
 		// mongodb local url
-		
-		String localDBURL ="mongodb://localhost:27017";
-		
-		mongoClient = MongoClients.create(localDBURL);
-		
+
+		String localDBURL = "mongodb://localhost:27017";
+
+//		mongoClient = MongoClients.create(localDBURL);
+
+		String atlasURL = "mongodb+srv://user2:2ciBx3H5wDh1jXp7@cluster0.ixxay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+		mongoClient = MongoClients.create(atlasURL);
+
 		// Get database and collection
 		database = mongoClient.getDatabase("sl-we-estore");
 		productsCollection = database.getCollection("products");
-		
-		
-		
+
 		// Let's insert a new product
 		Document product = new Document("name", "HP printer").append("price", 2500.25);
-		
+
 		productsCollection.insertOne(product);
-		
+
 		System.out.println(" successfully inserted a new product ");
 
 	}
