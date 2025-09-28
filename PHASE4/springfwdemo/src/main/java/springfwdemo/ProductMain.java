6e96972455e8cd953cd2a9c70f10976945307fc3
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-09-28 21:28:52 +0530

    spring fw dependency injection demo

--- PHASE4/springfwdemo/src/main/java/springfwdemo/ProductMain.java
+++ PHASE4/springfwdemo/src/main/java/springfwdemo/ProductMain.java
@@ -6,18 +6,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ProductMain {
 
 	public static void main(String[] args) {
-		
+
 		// IProduct p1 = new Product("TV", " 55 LED television", 55000.0f);
-		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+
+		System.out.println("\n\n Product Bean using constructor injection demo ");
 		IProduct p1 = (IProduct) context.getBean("product1");
-		
-		
+
 		System.out.println("Product p1's name is " + p1.getName());
 		System.out.println("Product p1's desc is " + p1.getDesc());
 		System.out.println("Product p1's price is " + p1.getPrice());
 
+		
+		System.out.println("\n\n Product Bean using setter injection demo ");
+		IProduct p2 = (IProduct) context.getBean("product2");
+
+		System.out.println("Product p2's name is " + p2.getName());
+		System.out.println("Product p2's desc is " + p2.getDesc());
+		System.out.println("Product p2's price is " + p2.getPrice());
 	}
 
 }
