63d618aade0158c7b99f9d1eb6389372976103d9
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-10-04 21:45:03 +0530

    spring aop before aspect demo

---
+++ PHASE4/aop-demo/src/main/java/com/sl/X.java
@@ -0,0 +1,14 @@
+package com.sl;
+
+public class X {
+
+	public void m1() {
+
+	}
+
+	public int doubleIt(int x) {
+
+		return 2*x;
+	}
+
+}
--- PHASE4/aop-demo/src/main/java/com/sl/aspects/LoggingAspect.java
+++ PHASE4/aop-demo/src/main/java/com/sl/aspects/LoggingAspect.java
@@ -8,18 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-    
-	//Aspect 1
-    @Before("execution(* com.sl.BankAccount.withdraw(..))")
-	public void withdrawAspect() {		
-		System.out.println("INSIDE withdrawAspect  Initiating withdrawl ....");		
+	// Aspect 1
+	@Before("execution(* com.sl.BankAccount.withdraw(..))")
+	public void withdrawAspect() {
+		System.out.println("INSIDE withdrawAspect  Initiating withdrawl ....");
 	}
-    
-    
-    //Aspect 2
-    @Before("execution(* com.sl.BankAccount.*(..))")
-	public void bankAcctAspect() {		
-		System.out.println("INSIDE bankAcctAspect  ....");		
+
+	// Aspect 2
+	@Before("execution(* com.sl.BankAccount.*(..))")
+	public void bankAcctAspect() {
+		System.out.println("INSIDE bankAcctAspect  ....");
 	}
+
+	// Aspect 3
+	// Task-1 : Write an aspect that will run only when getBalance of BankAccount is
+	// invoked
+	@Before("execution(* com.sl.BankAccount.getBalance( .. ))")
+	public void getBalanceAspect() {
+		System.out.println("INSIDE getBalance method");
+	}
+	
+	
 
 }
