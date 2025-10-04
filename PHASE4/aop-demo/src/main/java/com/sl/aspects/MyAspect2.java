a9bd58a0de532628250e0877048ceb1271d3bfa2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-10-04 22:43:16 +0530

    spring aop around aspect demo

--- PHASE4/aop-demo/src/main/java/com/sl/aspects/MyAspect2.java
+++ PHASE4/aop-demo/src/main/java/com/sl/aspects/MyAspect2.java
@@ -1,5 +1,6 @@
 package com.sl.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,16 +12,19 @@ public class MyAspect2 {
 
 	// Aspect 1
 	@Around("execution(* com.sl.BankAccount.deposit(..))")
-	public void aroundBankAccountDeposit() {
-		System.out.println("INSIDE MyAspect2 Aspect 1  aroundBankAccountDeposit ....");
-
+	public void aroundBankAccountDeposit(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("INSIDE MyAspect2 Aspect 1  aroundBankAccountDeposit BEFORE deposit....");
+		
+		pjp.proceed();
+		
+		System.out.println("INSIDE MyAspect2 Aspect 1  aroundBankAccountDeposit AFTER deposit....");
 	}
 
 	// Aspect 2
 	@AfterThrowing("execution(* com.sl.*.tripleIt(..))")
 	public void afterThrowingXTripleIt() {
 		System.out.println("INSIDE MyAspect2 Aspect 2  afterThrowingXTripleIt ....");
-
 	}
+	
 
 }
