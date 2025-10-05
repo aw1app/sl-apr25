983ec5067cb65a5d7a88763cb472a286086c7cd2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-10-05 22:03:19 +0530

    spring web mvc form processing demo

--- PHASE4/spring-web-mvc-demo/src/main/java/com/sl/controllers/ProductController.java
+++ PHASE4/spring-web-mvc-demo/src/main/java/com/sl/controllers/ProductController.java
@@ -9,18 +9,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/product-controller")
 public class ProductController {
-	
-	
-	@PostMapping("/add")
-	@ResponseBody
-	public String addProduct(@RequestParam("name") String name, @RequestParam("price") String price ) {
-		
-		System.out.println("name="+name + ", price="+price );
-		
-		
-		return " Added Product Succesfully";
+
+//	@PostMapping("/add")
+//	@ResponseBody
+//	public String addProduct(@RequestParam("name") String name, @RequestParam("price") String price ) {
+//		
+//		System.out.println("name="+name + ", price="+price );
+//		
+//		
+//		return " Added Product Succesfully";
+//	}
+
+	// Task-1 re implement above /add mapping where the response is rendered by a
+	// JSP.
+
+	@PostMapping("/add")	
+	public String addProduct(@RequestParam("name") String name, @RequestParam("price") String price) {
+		System.out.println("name of the product = " + name + ", price=" + price);
+		return "ProductAddMessage"; // --> JSP File Name
 	}
 
-	
-	// Task-1 re implement above /add mapping where the response is rendered by a JSP.
 }
