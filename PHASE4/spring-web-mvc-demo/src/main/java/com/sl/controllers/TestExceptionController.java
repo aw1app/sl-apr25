a302dc9baf852fecab3896206fb88b09c59e156c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-10-05 22:15:58 +0530

    spring web mvc form processing demo

---
+++ PHASE4/spring-web-mvc-demo/src/main/java/com/sl/controllers/TestExceptionController.java
@@ -0,0 +1,34 @@
+package com.sl.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestExceptionController {
+	
+	
+	@GetMapping("/1")
+	@ResponseBody
+	public String test1() {
+		
+		int x=10;
+		int y=0;
+		int result = x/y;
+		
+		
+		return "test 1 success! tesult = " + result ;
+	}
+	
+	
+	@ExceptionHandler(value = ArithmeticException.class)
+	@ResponseBody
+	public String expHandler() {
+		
+		return "Oops! somethingv went wrong!";
+	}
+
+}
