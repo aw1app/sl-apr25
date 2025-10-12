9ec94068a9e8ce090deadecd63196da6424d0a04
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-10-12 22:35:03 +0530

    spring boot data jpa

--- PHASE4/spring-data-jpa-demo/src/main/java/com/example/controller/ProductController.java
+++ PHASE4/spring-data-jpa-demo/src/main/java/com/example/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.Product;
 import com.example.repositry.ProductRepositry;
@@ -14,12 +16,9 @@ import com.example.repositry.ProductRepositry;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-	
-	
+
 	@Autowired
 	ProductRepositry productRepositry;
-	
-	
 
 	@GetMapping("/list-all")
 	public String getProducts(Model model) {
@@ -28,6 +27,14 @@ public class ProductController {
 		model.addAttribute("products", products);
 
 		return "products"; // views/products.jsp
+	}
+
+	// Task - 2
+	// Implement below functionality
+	@GetMapping("/delete/{id}")
+	public String addProduct(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
+
+		return "redirect:/products/list-all"; // WEB-INF/views/products.jsp
 	}
 
 }
